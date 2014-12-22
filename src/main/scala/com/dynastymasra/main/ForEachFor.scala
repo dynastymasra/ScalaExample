9a2eea7df4f012281ed1d92f18e36b22342f4657
dynastymasra <dynastymasra@gmail.com>
2014-12-22 15:59:25 +0700

    add for & for each

---
+++ src/main/scala/com/dynastymasra/main/ForEachFor.scala
@@ -0,0 +1,33 @@
+package com.dynastymasra.main
+
+/**
+ * Author   : @dynastymasra
+ * User     : Dimas Ragil T
+ * Email    : dynastymasra@gmail.com
+ * LinkedIn : http://www.linkedin.com/in/dynastymasra
+ * Blogspot : dynastymasra.wordpress.com | dynastymasra.blogspot.com
+ */
+class ForEachFor {
+  //http://khannedy.com/2013/08/31/perulangan-for-dan-foreach-di-scala/#more-5358
+  def testForEach(): Unit = {
+    println("Test for each in scala")
+    val data = List(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+    data.foreach(item => println(item))
+  }
+
+  def testFor(): Unit = {
+    println("Test for in scala")
+    val data = List(1, "Dimas", 2, "Ragil", 3, "T")
+    for (item <- data) {
+      println(item)
+    }
+  }
+}
+
+object main {
+  def main(args: Array[String]) {
+    val test = new ForEachFor()
+    test.testForEach()
+    test.testFor()
+  }
+}
