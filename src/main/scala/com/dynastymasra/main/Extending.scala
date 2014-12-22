f289426eb34e3c87ebeb292aac0f32d507ffa56d
dynastymasra <dynastymasra@gmail.com>
2014-12-22 16:56:28 +0700

    add extending class

---
+++ src/main/scala/com/dynastymasra/main/Extending.scala
@@ -0,0 +1,44 @@
+package com.dynastymasra.main
+
+/**
+ * Author   : @dynastymasra
+ * User     : Dimas Ragil T
+ * Email    : dynastymasra@gmail.com
+ * LinkedIn : http://www.linkedin.com/in/dynastymasra
+ * Blogspot : dynastymasra.wordpress.com | dynastymasra.blogspot.com
+ */
+class Extending(val xc: Int, val yc: Int) {
+
+  var x: Int = xc
+  var y: Int = yc
+
+  def move(dx: Int, dy: Int) {
+    x = x + dx
+    y = y + dy
+    println("Point x location : " + x);
+    println("Point y location : " + y);
+
+  }
+}
+
+class Location(override val xc: Int, override val yc: Int, val zc: Int) extends Extending(xc, yc) {
+  var z: Int = zc
+
+  def move(dx: Int, dy: Int, dz: Int) {
+    x = x + dx
+    y = y + dy
+    z = z + dz
+    println("Point x location : " + x);
+    println("Point y location : " + y);
+    println("Point z location : " + z);
+  }
+}
+
+object MainExtending {
+  //http://www.tutorialspoint.com/scala/scala_classes_objects.htm
+  def main(args: Array[String]) {
+    val main = new Location(10, 15, 20);
+    main.move(10, 10)
+    main.move(25, 30, 35)
+  }
+}
